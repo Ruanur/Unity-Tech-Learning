dcfd8b6e4b405f86f441592ecb12d9d82cb82605
Min Kyoung Jin <91407973+Ruanur@users.noreply.github.com>
2025-03-10 19:03:54 +0900

    이동

---
+++ Assets/Script/Attack/Melee.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Melee : MonoBehaviour
+{
+    public enum Type { Melee, Range };
+    public Type type;
+    public int damage;
+    public float rate;
+    public BoxCollider meleeArea;
+    public TrailRenderer trailEffect;
+
+}
--- Assets/Script/ClickMove.cs
+++ Assets/Script/ClickMove.cs
@@ -6,13 +6,15 @@ using UnityEngine.TextCore.Text;
 
 public class ClickMove : MonoBehaviour
 {
+    //마우스의 위치를 월드 좌표계로 표시하기 위해 선언
     private new Camera camera;
     private NavMeshAgent agent;
     private Animator anim;
 
+    //캐릭터가 움직이고 있는지 확인
     private bool isMove;
+    //캐릭터 목표 위치 기억
     private Vector3 destination;
-    [SerializeField] private Transform character;
 
     private void Awake()
     {
@@ -23,9 +25,10 @@ public class ClickMove : MonoBehaviour
 
     private void Start()
     {
-        anim = character.GetComponent<Animator>();    
+        anim = GetComponent<Animator>();    
     }
 
+    
     void Update()
     {
         if (Input.GetMouseButton(1))
@@ -40,6 +43,7 @@ public class ClickMove : MonoBehaviour
         LookMoveDirection();
     }
 
+    //클릭으로 찾아낸 마우스의 위치를 저장 후 이동을 시작하게 하는 역할 수행, Nav 시스템 활용
     private void SetDestination(Vector3 dest)
     {
         agent.SetDestination(dest);
@@ -47,18 +51,22 @@ public class ClickMove : MonoBehaviour
         isMove = true;
     }
 
+    //플레이어 회전 함수
     private void LookMoveDirection()
     {
+        //속도가 0일 때, 이동을 중지
         if (agent.velocity.magnitude == 0f)
         {
             isMove = false;
             anim.SetBool("Move", false);
             return;
         }
+
+        //이동 중, 방향 설정
         if (isMove)
         {
             var dir = new Vector3(agent.steeringTarget.x, transform.position.y, agent.steeringTarget.z) - transform.position;
-            character.transform.forward = dir;
+            transform.forward = dir;
             anim.SetBool("Move", true);
             //transform.position += dir.normalized * Time.deltaTime * 5f;
         }
