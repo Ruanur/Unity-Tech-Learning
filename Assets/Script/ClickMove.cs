1641027406e52d2f906301d4388666a36c54e87a
Min Kyoung Jin <91407973+Ruanur@users.noreply.github.com>
2025-03-14 03:48:44 +0900

    Click Move 코드 수정

--- Assets/Script/ClickMove.cs
+++ Assets/Script/ClickMove.cs
@@ -6,89 +6,58 @@ using UnityEngine.TextCore.Text;
 
 public class ClickMove : MonoBehaviour
 {
-    //마우스의 위치를 월드 좌표계로 표시하기 위해 선언
     private new Camera camera;
     private NavMeshAgent agent;
     private Animator anim;
 
-    //캐릭터가 움직이고 있는지 확인
     private bool isMove;
-    //캐릭터 목표 위치 기억
     private Vector3 destination;
+    [SerializeField] private Transform character;
 
-    bool isAttackDelay;
-    float AttackDelay;
-    M1 equipWeapon;
-
-    
     private void Awake()
     {
         camera = Camera.main;
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
+        anim = character.GetComponent<Animator>();
     }
 
-    private void Start()
-    {
-        anim = GetComponent<Animator>();    
-    }
-
-    
     void Update()
     {
         if (Input.GetMouseButton(1))
         {
-            RaycastHit hit;
-            if (Physics.Raycast(camera.ScreenPointToRay(Input.mousePosition), out hit))
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit))
             {
                 SetDestination(hit.point);
+                isMove = true;
             }
         }
-
         LookMoveDirection();
-        Attack();
     }
 
-    //클릭으로 찾아낸 마우스의 위치를 저장 후 이동을 시작하게 하는 역할 수행, Nav 시스템 활용
     private void SetDestination(Vector3 dest)
     {
         agent.SetDestination(dest);
         destination = dest;
-        isMove = true;
+        
     }
 
-    //플레이어 회전 함수
     private void LookMoveDirection()
     {
-        //속도가 0일 때, 이동을 중지
         if (agent.velocity.magnitude == 0f)
         {
             isMove = false;
             anim.SetBool("Move", false);
             return;
         }
-
-        //이동 중, 방향 설정
         if (isMove)
         {
             var dir = new Vector3(agent.steeringTarget.x, transform.position.y, agent.steeringTarget.z) - transform.position;
-            transform.forward = dir;
+            character.transform.forward = dir;
             anim.SetBool("Move", true);
             //transform.position += dir.normalized * Time.deltaTime * 5f;
         }
 
-    }
-
-    void Attack()
-    {
-        AttackDelay += Time.deltaTime;
-        isAttackDelay = equipWeapon.rate < AttackDelay;
-
-        if (Input.GetMouseButton(0))
-        {
-            equipWeapon.Use();
-            anim.SetTrigger("doSwing");
-            AttackDelay = 0;
-        }
     }
 }
